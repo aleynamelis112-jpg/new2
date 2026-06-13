834ad92861ca6afdddc052401d17363d29843c76
aleynamelis112-jpg <aleynamelis112@gmail.com>
2026-06-13 20:33:51 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,30 @@
-import type { NextConfig } from 'next';
+
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
   typescript: {
     ignoreBuildErrors: true,
   },
-
   eslint: {
     ignoreDuringBuilds: true,
   },
-
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
